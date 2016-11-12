f8d31922567032d2ef0510d91cb2548f1dc08128
klaskagan <klaskagan@gmail.com>
2016-11-12 16:37:00 +0200

    Routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UsersComponent,
+    PostsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: 'home', component: HomeComponent},
+      {path: 'users', component: UsersComponent},
+      {path: 'posts', component: PostsComponent},
+      {path: '**', redirectTo: '/home'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
